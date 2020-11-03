fe44523a410ace38adcb08973504ecc9673e7bda
emad samy <emadsamy3030@gmail.com>
2020-11-03 23:34:44 +0200

    init

--- iaoshe/assets/js/functions.js
+++ iaoshe/assets/js/functions.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
     $("#menuBtn").on('click', function () {
         $('.nav-content').toggleClass('active');
+        $(this).toggleClass('active');
     });
     $("#closeNavbar").on('click', function () {
         $('.nav-content').removeClass('active');
+        $("#menuBtn").removeClass('active');
     });
 });
