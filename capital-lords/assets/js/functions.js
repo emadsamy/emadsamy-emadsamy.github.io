332f52ab5b30470c52abec617b370aac02c7c114
emad samy <emadsamy3030@gmail.com>
2021-05-07 00:10:34 +0200

    init

--- capital-lords/assets/js/functions.js
+++ capital-lords/assets/js/functions.js
@@ -185,3 +185,46 @@ $(window).on("resize", function () {
   // Menu
   menuBodyHandler();
 });
+
+// Products Swiper
+new Swiper("#productsSwiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 4,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 4000,
+  },
+  speed: 1000,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".arrow-next",
+    prevEl: ".arrow-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1.8,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+});
